45dbbf2426c44927d6406f33b146c49b690d362c
lyx <871723950@qq.com>
2020-12-11 19:34:52 +0800

    lyx
    init project

---
+++ lyxpjone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lyxpjone
+//
+//  Created by lyx on 2020/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
